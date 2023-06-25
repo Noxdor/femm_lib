3418fd705276060b3377cb60e87f0ff0f26e377a
Lennart Guderian <lennart@guderian.de>
2023-06-25 09:26:50 +0200

    improves file_io and tests that it's working

--- file_io.lua
+++ file_io.lua
@@ -1,21 +1,23 @@
+Util = dofile("femm_lib/util.lua")
+
 local module = {}
 
 -- Accepts a student id (0..99) and reads in the simulation values from
 -- a file of the name `Input_<student_id>.txt`. The names of the simulation values
 -- are given as a table and they are read in the order they were specified in the table.
+-- The values are written into the same table with each values name as the key. They can
+-- be accessed on the same table after this function returned.
 -- This function enables access to optimisation functionality with OptiY.
 function module.read_input(student_id, values)
 	local inputFileName = format("Input_%1$02d.txt", student_id)
 	local inputFile = openfile(inputFileName, "r")
 
-	local result = {}
-
 	-- if file was opened successfully, read in data
 	if inputFile then
-		for i = 1, values.n do
+		for i = 1, Util.length(values) do
 			local value = values[i]
 			read(inputFile, "*l")
-			result[value] = read(inputFile, "*l", "*n")
+			values[value] = read(inputFile, "*l", "*n")
 		end
 
 		closefile(inputFile)
@@ -29,9 +31,10 @@ function module.write_output(student_id, values)
 	outputFile = openfile(outputFileName, "w") -- Output-Datei in LUA-Ordner
 
 	if outputFile then
-		for value in values do
-			output_line = format("%s=%d", value.name, value.val)
-			write(outputFile, "R=", output_line) -- Name und Wert auf separate Zeilen
+		for i = 1, Util.length(values) do
+			local value = values[i]
+			local output_line = format("%s=\n%f\n", value.name, value.val)
+			write(outputFile, output_line) -- Name und Wert auf separate Zeilen
 		end
 	end
 	closefile(outputFile)
